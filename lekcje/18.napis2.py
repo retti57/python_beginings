3aac49e59b4ad6fd39d25d65e0cbe51ba30914eb
retti57 <118014236+retti57@users.noreply.github.com>
2023-01-04 22:01:14 +0100

    Delete 18.napis2.py

--- lekcje/18.napis2.py
+++
@@ -1,8 +0,0 @@
-word = input('Podaj napis: ')
-count = 0
-search = input('Podaj literę której szukamy: ')
-for letter in word:
-    if search == letter:
-        count += 1
-
-print('Szukana fraza', search, 'występuje ', count, 'razy')
