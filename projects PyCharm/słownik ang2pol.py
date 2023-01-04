1c5fdda3b78b8ea10422c9f8380dc698f7fe624b
Czaja <dczajka1@gmail.com>
2023-01-04 20:18:46 +0100

    Chyba czwarty commit

--- projects PyCharm/słownik ang2pol.py
+++ projects PyCharm/słownik ang2pol.py
@@ -1,18 +1,19 @@
-from string import punctuation
-import os
-
+import string
 try:
-    fhand = open('ang2pol.txt', mode = 'a', encoding = 'UTF-8')  # metoda 'a' - dodaje na koniec do pliku
+    fhand = open('ang2pol.txt', mode='a', encoding='UTF-8')  # metoda 'a' - dodaje na koniec do pliku
     ang2pol = dict()
     while True:
         sth = input('Czy chcesz coś dodać?: ')
+        sth_clean = sth.translate(str.maketrans('', '', string.punctuation))
+        sth_clean_lower = sth_clean.lower()
+        # words = line.split()
         if sth == 'nie' or sth == 'n' or sth == 'n' or sth == 'no':
             print('Nic więcej nie dodajesz.')
             break
         elif sth == 'tak' or sth == 't' or sth == 'y' or sth == 'yes':
             ang = input('Podaj angielskie słówko: ')
             pol = input('Podaj polskie tłumaczenie: ')
-            fhand.write(f"{ang}, {pol}"+os.sep)
+            fhand.write(f"{ang}, {pol}")
             ang2pol[ang] = pol
             continue
         else:
@@ -23,5 +24,3 @@ try:
 finally:
     print("zamykam plik bo finally")
     fhand.close()
-
-def dodajAng():
--- projects PyCharm/słownik ang2pol_v2.py
+++ projects PyCharm/słownik ang2pol_v2.py
@@ -1,4 +1,3 @@
-
 def openFile(filename: str):
     return open(filename, mode='r+', encoding='UTF-8')  # metoda 'a' - dodaje na koniec do pliku
 
@@ -14,14 +13,24 @@ def readDataFromFileToDictionary(file):
 def readNewWords(localDict):
     newWords = dict()
     sth = input('Czy chcesz coś dodać?: ')
+    #editingInput(sth)
     approvedChoice = ['tak', 't', 'y', 'yes']
     while sth in approvedChoice:
         ang = input('Podaj angielskie słówko: ')
+        #editingInput(ang)
         pol = input('Podaj polskie tłumaczenie: ')
+        #editingInput(pol)
         addNewWordIfNotExist(ang,pol, localDict, newWords)
         sth = input('Czy chcesz coś dodać?: ')
     return newWords
 
+'''
+def editingInput(inp: str):
+    import string
+    inp2 = inp.lower()
+    word = inp2.translate(str.maketrans("","",string.punctuation))
+    return word
+'''
 
 def addNewWordIfNotExist(ang,pol,localDict,newWords):
     if ang not in localDict:
@@ -39,6 +48,7 @@ def main():
     nnew = readNewWords(localDict)
     saveNewWordsInFile(file, nnew)
     file.close()
+
 
 
 main()
